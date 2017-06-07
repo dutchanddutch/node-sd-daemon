c62097598bbe647826818e4272c8c3fb05604ac8
Matthijs van Duin <matthijsvanduin@gmail.com>
2017-06-07 06:17:57 +0200

    add watchdog support

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 
 const notify	= require('./lib/notify');
 const fds	= require('./lib/fds');
+const watchdog	= require('./lib/watchdog');
 
-module.exports = { notify, fds };
+module.exports = { notify, fds, watchdog };
