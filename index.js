a9dc97e58b8420734f21dc1fd6f98e38b1520ca3
Matthijs van Duin <matthijsvanduin@gmail.com>
2017-07-10 13:12:11 +0200

    small cleanup

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const notify	= require('./lib/notify');
-const fds	= require('./lib/fds');
-const watchdog	= require('./lib/watchdog');
-
-module.exports = { notify, fds, watchdog };
